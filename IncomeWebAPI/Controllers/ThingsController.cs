ff5ab6a80fb97536e15d5c677f34fe432b621420
bibookj <1michalbaran@gmail.com>
2020-07-04 11:39:05 +0200

    feat: initial commit

---
+++ IncomeWebAPI/Controllers/ThingsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace IncomeWebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ThingsController : ControllerBase
+    {
+        private static List<string> thingsList = new List<string> { "Banana", "Computer", "Mouse", "Mug" };
+
+        [HttpGet]
+        public List<string> GetThings()
+        {
+            return thingsList;
+        }
+
+        [HttpGet("{id}")]
+        public string GetThingById([FromRoute] int id)
+        {
+            return thingsList[id - 1];
+        }
+
+        [HttpPost]
+        public IActionResult AddThing([FromBody]string thing)
+        {
+            thingsList.Add(thing);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateThing([FromRoute] int id,[FromBody]string thing)
+        {
+            thingsList[id - 1] = thing;
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteThing([FromRoute] int id)
+        {
+            thingsList.RemoveAt(id - 1);
+            return Ok();
+        }
+    }
+}
---
+++ IncomeWebAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace IncomeWebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("pogoda")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+      
+
+        [HttpGet("even")]
+        public List<int> EvenNumbers(int amount = 10)
+        {
+            List<int> list = new List<int>();
+            for (int i = 1; i <= amount; i++)
+            {
+                list.Add(i*2);
+            }
+            
+            return list;
+        }
+    }
+}
